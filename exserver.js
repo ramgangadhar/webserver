e517620d1f90e39a6a1772089417bc509c3dba7f
ram gangadhar <nanivenkat789@gmail.com>
2016-09-14 11:36:04 +0530

    one file is updated

--- exserver.js
+++ exserver.js
@@ -4,7 +4,7 @@ app.get('/',function (reg,res){
 	res.send('heloo you r using express for runing the server');
 })
 app.get('/about',function (reg,res){
-	res.send('heloo you r using express ');
+	res.send('heloo you r using express  for connecting to the server');
 })
 app.listen(4040);
 

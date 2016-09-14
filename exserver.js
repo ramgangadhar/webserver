cf22f8b77ae211a402bf22114100bf00328d7079
ram gangadhar <nanivenkat789@gmail.com>
2016-09-14 12:56:46 +0530

    updated files

--- exserver.js
+++ exserver.js
@@ -1,10 +1,11 @@
 var express=require('express');
 var app=express();
+var port=process.env.PORT || 8080;
 app.get('/',function (reg,res){
 	res.send('heloo you r using express for runing the server');
 })
 app.get('/about',function (reg,res){
 	res.send('heloo you r using express  for connecting to the server');
 })
-app.listen(4040);
+app.listen(port);
 
--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 var express= require('express');
 var app=express();
 var port=process.env.PORT || 8080;
+
 var middeleware=require('./middleware1.js')
 
 app.use(middeleware.logger);
@@ -9,9 +10,9 @@ app.get('/about',middeleware.requireAuthentication,function(req,res){
 	res.send('this is about the page')
 })
 app.use(middeleware.logger);
-/*app.get('/aboutus',middeleware.logger,function(req,res){
+app.get('',function(req,res){
 	res.send('this is about us  the page')
-})*/
+})
 app.use(express.static(__dirname+'/public'))
 app.listen(port,function () {
 	console.log('express server started on port'+port);
